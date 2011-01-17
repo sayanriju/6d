233686b8b103e33132b1b55074f8cedf55b669aa
Joey Guerra <junkmail66@mac.com>
2011-01-16 22:22:47 -0600

    Styled the delete form at the bottom of each post in the post listing. Updated index_admin namings.

--- js/index_admin.js
+++ js/index_admin.js
@@ -1,5 +1,8 @@
-sixd.view.index = function(){
-	sixd.view.apply(this, [document]);
+sixd.ui = function(){
+	sixd.apply(this, [this]);
+}
+sixd.ui.home = function(){
+	sixd.ui.apply(this, [document]);
 	var cached_elem = null;
 	var textarea = SDDom.create('textarea');
 	this.home_page = new sixd.model.home_page();
@@ -31,19 +34,19 @@ sixd.view.index = function(){
 	this.listen_for(textarea, 'blur', save);
 }
 
-sixd.controller.index = function(view){
+sixd.controller.home = function(view){
 	sixd.controller.apply(this, [view]);
 	this.home_page = new sixd.model.home_page();
 	var self = this;
 	view.add_subscriber(this, 'field_should_save');
 }
-sixd.controller.index.prototype.field_should_save = function(publisher, info){
+sixd.controller.home.prototype.field_should_save = function(publisher, info){
 	this.save(info);
 };
-sixd.controller.index.prototype.success = function(data, status, request, type){
+sixd.controller.home.prototype.success = function(data, status, request, type){
 	alert(status);
 };
-sixd.controller.index.prototype.save = function(home_page){
+sixd.controller.home.prototype.save = function(home_page){
 	this.home_page = home_page;
 	jQuery.ajax('view', home_page);
 };
@@ -52,6 +55,11 @@ sixd.model.home_page = function(){
 	this.html = null;
 };
 sixd.main(function(e){
-	var index_view = new sixd.view.index();
-	new sixd.controller.index(index_view);
+	// include jquery.
+	var script = document.createElement('script');
+	script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js';
+	script.type = 'text/javascript';
+	SDDom.insertBefore(script, document.getElementsByTagName('script')[0]);	
+	var index_view = new sixd.ui.home();
+	new sixd.controller.home(index_view);
 });
